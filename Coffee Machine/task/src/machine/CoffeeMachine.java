2c933dccabe121dacdfc40569eac29e42f39ee11
Anastasia <anastasiavlg@mail.ru>
2021-12-04 22:49:01 +0300

    Display ingredients for the given number of cups of coffee

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,8 +1,28 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
-        makeCoffee();
+        displayIngredients(askCupNumber(new Scanner(System.in)));
+    }
+
+    public interface AmountPerCup {
+        int MILK = 50;
+        int WATER = 200;
+        int COFFEE = 15;
+    }
+
+    private static void displayIngredients(int cupNumber) {
+        System.out.printf("For %d cups of coffee you will need:\n", cupNumber);
+        System.out.printf("%d ml of water\n", AmountPerCup.WATER * cupNumber);
+        System.out.printf("%d ml of milk\n", AmountPerCup.MILK * cupNumber);
+        System.out.printf("%d g of coffee beans\n", AmountPerCup.COFFEE * cupNumber);
+    }
+
+    private static int askCupNumber(Scanner scanner) {
+        System.out.println("Write how many cups of coffee you will need:");
+        return scanner.nextInt();
     }
 
     private static void makeCoffee() {
